a49aeb5dcefac6ad19c26e6e2cd6f3eef31cce4f
venturethai <venturethaihq@gmail.com>
2026-05-24 13:40:39 +0700

    added search feature

--- script.js
+++ script.js
@@ -3,3 +3,27 @@ const darkBtn = document.getElementById("darkModeBtn");
 darkBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark");
 });
+
+/* SEARCH */
+
+const searchBar = document.getElementById("searchBar");
+
+searchBar.addEventListener("keyup", () => {
+
+    const searchValue = searchBar.value.toLowerCase();
+
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach(card => {
+
+        const title = card.querySelector("h2").textContent.toLowerCase();
+
+        if(title.includes(searchValue)){
+            card.style.display = "block";
+        }else{
+            card.style.display = "none";
+        }
+
+    });
+
+});
